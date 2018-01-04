d5cca74b1728585a0f0c091441c72d7d8bb5e7cf
Jake Runzer <jakerunzer@gmail.com>
2018-01-04 01:08:27 -0800

    jakerunzer.com

--- src/View.elm
+++ src/View.elm
@@ -66,7 +66,7 @@ footer =
             [ text "Made with "
             , span [ class "heart" ] [ text "♥" ]
             , text " by "
-            , a [ href "https://jakerunzer.xyz" ] [ text "jakerunzer" ]
+            , a [ href "https://jakerunzer.com" ] [ text "jakerunzer" ]
             ]
         ]
 
